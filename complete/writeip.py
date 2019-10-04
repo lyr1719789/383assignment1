7cb492b2a16be0d241223160038b9fbc21f9c15d
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-04 16:10:30 +1300

    Update writeip.py

--- complete/writeip.py
+++ complete/writeip.py
@@ -2,3 +2,9 @@ a=input("my google sql ip:")
 modified= open('ip.txt',"w")
 modified.write(a)
 modified.close()
+
+
+b=input("my phpmyadmin site")
+website= open('help/cgi-bin/website.txt',"w")
+website.write("'"+b+"'")
+website.close()
