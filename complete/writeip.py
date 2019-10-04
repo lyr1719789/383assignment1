cbb005c8f4364ab7ed95828be6bc40b39fadca82
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-04 23:06:12 +1300

    Update writeip.py

--- complete/writeip.py
+++ complete/writeip.py
@@ -3,8 +3,3 @@ modified= open('ip.txt',"w")
 modified.write(a)
 modified.close()
 
-
-b=input("my phpmyadmin site")
-website= open('help/cgi-bin/website.txt',"w")
-website.write("'"+b+"'")
-website.close()
