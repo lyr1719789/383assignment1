f66e22f9b90a755df7a2aeeefa4211980ef96eea
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-06 00:28:33 +1300

    Update teacher.py

--- complete/help/cgi-bin/teacher.py
+++ complete/help/cgi-bin/teacher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print("Content-type:text/html\n\n")
+
 import sys
 sys.path.append("H:\\apps\\Python27\\lib\\site-packages")
 import cgi
@@ -7,6 +7,10 @@ import os
 import csv
 import pymysql
 
+
+print("Content-type:text/html\n\n")
+print("<title>Result</title>")
+print("<body><center>")
 
 
 def Student_list(file_name):
@@ -18,47 +22,55 @@ def Student_list(file_name):
         for row in reader:
             list_of_students.append(row)
             # Remove metadata from top row
-        list_of_students.pop(0)
+
     return list_of_students
 
-def modify_csv(num,id,password):
-    students = Student_list("StudentDatas.csv")
-    if num=="ftp":
-        students[id-1][6]=password
-    elif  num=="phpmyadmin":
-        students[id - 1][5]=password
-    else:
-        return
+def modify_csv(num,students,id, password):
+    a=students
+    count=0
+    if num == "ftp":
+        for i in students:
+            if i[0]==id:
+                a[count][6]=password
+            count=count+1
+    elif num == "phpmyadmin":
+        for i in students:
+            if i[0]==id:
+                a[count][5]=password
+            count=count+1
+
     os.system("sudo rm -rf StudentDatas.csv")
-    with open('StudentDatas.csv', 'w', newline='') as csvfile:
+    with open('StudentDatas.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
 
-        for row in students:
+        for row in a:
             writer.writerow(row)
 
-def confirm_permission(student_list,teacher, app, username, pwd):
+def confirm_permission(student_list,teacher, app, username, pwd,sid):
     count=0
     for student in student_list:
+
         if teacher == student[3]:
             if app == "ftp":
                 if username==student[0]:
                     ftp_change(username, pwd)
-                    print("Modify successfully!  (*^▽^*)")
+                    print("Modify successfully!")
+                    modify_csv(app,student_list,sid, pwd)
 
                 else:
                     print("Please check your account!")
-
             elif app == "phpmyadmin":
                 if username == student[4]:
                     phpma_change(username, pwd)
-                    print("Modify successfully!  (*^▽^*)")
+                    print("Modify successfully! ")
+                    modify_csv(app,student_list,sid, pwd)
                 else:
                     print("Please check your account!")
 
             elif app == "moodle":
                 if username == student[4]:
                     moodle_change(username, pwd)
-                    print("Modify successfully!  (*^▽^*)")
+                    print("Modify successfully!")
                 else:
                     print("Please check your account!")
         else:
@@ -67,14 +79,14 @@ def confirm_permission(student_list,teacher, app, username, pwd):
         if count==len(student_list):
             print("Your email does not exist,please check it!")
     print("""
-            <form action="../student.html" method="GET">
+            <form action="../modify.html" method="GET">
                             <input type="submit" value="Back to home page">
                         </form>
                         """)
 
 
 def ftp_change(username, pwd):
-    os.system("sudo echo s1:1234| chpasswd")
+    os.system("sudo echo %s:%s| sudo chpasswd"%(username,pwd))
 
 
 def phpma_change(username, pwd):
@@ -115,9 +127,6 @@ def moodle_change(username,pwd):
     print(cursor.rowcount, "record(s) affected")
 
 
-print("<title>Result</title>")
-print("<body><center>")
-
 form = cgi.FieldStorage()
 sid = form['sid'].value if 'sid' in form else ''
 teacher = form['teacher'].value if 'teacher' in form else ''
@@ -125,7 +134,6 @@ app = form['app'].value if 'app' in form else ''
 username = form['account'].value if 'account' in form else ''
 pwd = form['pwd'].value if 'pwd' in form else ''
 students=Student_list("StudentDatas.csv")
-confirm_permission(students,teacher, app, username, pwd)
-modify_csv(app,sid,pwd)
-print(app)
+confirm_permission(students,teacher, app, username, pwd,sid)
+                                                             
 print('</center></body>')
