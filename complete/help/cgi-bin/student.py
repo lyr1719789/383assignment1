ec2d877ad118f6f70692ee52b30e8d187fe0ff1b
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-05 17:58:59 +1300

    Update student.py

--- complete/help/cgi-bin/student.py
+++ complete/help/cgi-bin/student.py
@@ -2,19 +2,36 @@
 import sys
 sys.path.append("H:\\apps\\Python27\\lib\\site-packages")
 import cgi
+import csv
 
 
-list=[["try.html",0,9]]
-def match_site(name):
-    for i in list:
-        if i[0] ==name:
+
+def Student_list(file_name):
+    with open(file_name, 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        # create empty list
+        list_of_students = []
+        # for each student, append as list to list (list of lists)
+        for row in reader:
+            list_of_students.append(row)
+            # Remove metadata from top row
+        list_of_students.pop(0)
+    return list_of_students
+
+def match_site(slist,name):
+    count=0
+    for i in slist:
+        if i[4] ==name:
             print('<script type="text/javascript"> ')
             print("location.href ='http://' + window.location.href.split('/')[2]+'/'+'%s';"%name)
             print('</script>')
 
         else:
-            print("Your site name is wrong ,please check it")
-            print("""
+            count=count+1
+    if count==len(slist):
+
+         print("Your site name is wrong ,please check it")
+         print("""
             <form action="../student.html" method="GET">
                 <input type="submit" value="Back to Home page">
             </form>
@@ -29,6 +46,7 @@ print("<body><center>")
 
 form = cgi.FieldStorage()
 name = form['id'].value if 'id' in form else ''
-match_site(name)
-
+students=Student_list("StudentDatas.csv")
+match_site(students,name)
 print('</center></body>')
+                               
