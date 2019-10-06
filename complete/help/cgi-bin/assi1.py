58450686eebf4fdb92c3b04f07f4b828b2a1d0da
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-06 17:05:08 +1300

    Update assi1.py

--- complete/help/cgi-bin/assi1.py
+++ complete/help/cgi-bin/assi1.py
@@ -8,6 +8,9 @@ import os
 import csv
 import string
 import pymysql
+print("Content-type: text/html\n")
+print("<title>Assignment1 progress<</title>")
+
 r = os.popen("curl ifconfig.me")
 exip = r.read()
 r.close()
@@ -24,8 +27,7 @@ with open('StudentDatas.csv', 'r') as csvFile:
                 list_of_students.append(row)
                 # Remove metadata from top row
         list_of_students.pop(0)
-print("Content-type: text/html\n")
-print("<title>Assignment1 progress<</title>")
+
 print("<head>")
 print("<style type='text/css'>")
 print("th,td{border:2px solid black;}")
