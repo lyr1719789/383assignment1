aeca7413e7c4657204967aa9c0ce50252329a981
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-06 15:24:40 +1300

    Add files via upload

--- complete/help/cgi-bin/assi2.py
+++ complete/help/cgi-bin/assi2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import sys
 sys.path.append("H:\\apps\\Python27\\lib\\site-packages")
 import cgi
@@ -7,17 +6,15 @@ import os
 # read the csv file exp file:csv_example new csv file:csv_examples
 import csv
 import string
-
 import pandas as pd
 import pymysql
-
 r = os.popen("curl ifconfig.me")
 exip = r.read()
 r.close()
 f = open("ip.txt","r")
 ip = f.read()
-f.close() 
-
+sqlip = ip.replace("\n","")
+f.close()
 with open('StudentDatas.csv', 'r') as csvFile:
         reader = csv.reader(csvFile)
         # create empty list
@@ -27,9 +24,6 @@ with open('StudentDatas.csv', 'r') as csvFile:
                 list_of_students.append(row)
                 # Remove metadata from top row
         list_of_students.pop(0)
-
-		
-		
 print("Content-type: text/html\n")
 print("<title>Assignment2 progress</title>")
 print("<head>")
@@ -37,8 +31,12 @@ print("<style type='text/css'>")
 print("th,td{border:2px solid black;}")
 print("table{padding: 60px 50px;}")
 print("input:hover { background-color:green; /* Green */    color: white;border-radius: 50%;}")
-print("input {color: green; text-align: left; text-decoration: none; display: inline-block; font-size: 16px;border: 2px solid #4CAF50;border-radius: 50%;}")
-print("body {background:url('http://blog.hostbaby.com/wp-content/uploads/2014/03/PaintSquares_1400x900-1024x658.jpg');background-size:98% 100%;background-repeat:no-repeat; }")
+print("button:hover { background-color:green; /* Green */    color: white;border-radius: 50%;}")
+print("button:hover { background-color:green; /* Green */    color: white;border-radius: 50%;}")
+print("input,button {color: green; text-align: left; text-decoration: none; display: inline-block; font-size: 16px;bor
+der: 2px solid #4CAF50;border-radius: 50%;}")
+print("body {background:url('http://blog.hostbaby.com/wp-content/uploads/2014/03/PaintSquares_1400x900-1024x658.jpg');
+background-size:98% 100%;background-repeat:no-repeat; }")
 print("h1{text-shadow: 5px 5px 5px #95CACA;text-align:center;text-decoration:overline;letter-spacing:2px;")
 print("</style>")
 print("</head>")
@@ -46,34 +44,38 @@ print("<body><center>")
 print("<table>")
 print("<tr><th>ID/task</th><th>first</th></tr>")
 for i in list_of_students:
-	print("<script type='text/javascript'>")
-	print("var btn = document.getElementById('btn')")
-	print("btn.onclick=function(){window.location.href='http://'+str(exip)+'/'+str(i[4])+''}")
-	print("<tr>")
-	print("<td><button id='btn'>student'+str(i[0])+'</button></td>")
+        print("<tr>")
+        print("<td><a href='http://%s/%s' target='_blank'>student%s</a></td>"%(exip,str(i[4]),str(i[0])))
 # get post data
-	form = cgi.FieldStorage()
-	# query to check password and get permissions
-	sql = 'SELECT 1 FROM mdl_glossary_entries'
-	sq1= 'SELECT 1 FROM mdl_glossary_categories'
-	# connect to database
-	conn = pymysql.connect(host=ip,user="root",password="Moodle123moodle",db =""+str(i[4])+"",charset ="utf8")
-	#1
-	cursor = conn.cursor()
-	cursor.execute(sql)
-	entries = cursor.fetchall()
-	cursor1 = conn.cursor()
-	cursor1.execute(sq1)
-	cate=cursor1.fetchall()
-	if len(entries)>= 3 and len(cate)>=2:
-		print("<th>ok</th>")
-	else:
-		print("<th>no</th>")
-		print("</tr>")
+        form = cgi.FieldStorage()
+        # query to check password and get permissions
+        sql = 'SELECT 1 FROM mdl_glossary_entries'
+        sq1= 'SELECT 1 FROM mdl_glossary_categories'
+        # connect to database
+        conn = pymysql.connect(host=sqlip,user="root",password="Moodle123moodle",db ="student%s"%str(i[0]),charset ="u
+tf8")
+        #1
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        entries = cursor.fetchall()
+        cursor1 = conn.cursor()
+        cursor1.execute(sq1)
+        cate=cursor1.fetchall()
+        if len(entries)>= 3 and len(cate)>=2:
+                print("<th>ok</th>")
+        else:
+                print("<th>no</th>")
+                print("</tr>")
 print("</table>")
 print("""
 <form action="../login.html" method="GET">
     <input type="submit" value="Back to progress">
 </form>
 """)
+print("<td><button id='btn'>phpmyadmin</button></td>")
+print("<script type='text/javascript'>")
+print("var btn = document.getElementById('btn')")
+print("btn.onclick=function(){window.location.href='http://%s/phpMyAdmin'}"%exip)
+print("</script>")
+print("<h3>You can check all the changes in their moodle site</h3>")
 print('</center></body>')
--- complete/help/cgi-bin/assi4.py
+++ complete/help/cgi-bin/assi4.py
@@ -7,17 +7,14 @@ import os
 # read the csv file exp file:csv_example new csv file:csv_examples
 import csv
 import string
-
 import pandas as pd
 import pymysql
-
 r = os.popen("curl ifconfig.me")
 exip = r.read()
 r.close()
 f = open("ip.txt","r")
 ip = f.read()
-f.close() 
-
+f.close()
 with open('StudentDatas.csv', 'r') as csvFile:
         reader = csv.reader(csvFile)
         # create empty list
@@ -27,8 +24,6 @@ with open('StudentDatas.csv', 'r') as csvFile:
                 list_of_students.append(row)
                 # Remove metadata from top row
         list_of_students.pop(0)
-
-
 print("Content-type: text/html\n")
 print("<title>Assignment4 progress</title>")
 print("<head>")
@@ -36,8 +31,11 @@ print("<style type='text/css'>")
 print("th,td{border:2px solid black;}")
 print("table{padding: 60px 50px;}")
 print("input:hover { background-color:green; /* Green */    color: white;border-radius: 50%;}")
-print("input {color: green; text-align: left; text-decoration: none; display: inline-block; font-size: 16px;border: 2px solid #4CAF50;border-radius: 50%;}")
-print("body {background:url('http://blog.hostbaby.com/wp-content/uploads/2014/03/PaintSquares_1400x900-1024x658.jpg');background-size:98% 100%;background-repeat:no-repeat; }")
+print("button:hover { background-color:green; /* Green */    color: white;border-radius: 50%;}")
+print("input,button{color: green; text-align: left; text-decoration: none; display: inline-block; font-size: 16px;bord
+er: 2px solid #4CAF50;border-radius: 50%;}")
+print("body {background:url('http://blog.hostbaby.com/wp-content/uploads/2014/03/PaintSquares_1400x900-1024x658.jpg');
+background-size:98% 100%;background-repeat:no-repeat; }")
 print("h1{text-shadow: 5px 5px 5px #95CACA;text-align:center;text-decoration:overline;letter-spacing:2px;")
 print("</style>")
 print("</head>")
@@ -45,32 +43,32 @@ print("<body><center>")
 print("<h1>Progress of Assignment4</h1>")
 print("<table>")
 print("<tr><th>ID/task</th><th>first</th><th>second</th></tr>")
-
-print("</script>")
 for i in list_of_students:
-	print("<script type='text/javascript'>")
-	print("var btn = document.getElementById('btn')")
-	print("btn.onclick=function(){window.location.href='http://'+str(exip)+'/'+str(i[4])+''}")
-	print("<tr>")
-	print("<td><button id='btn'>student'+str(i[0])+'</button></td>")
+        print("<tr>")
+        print("<td><a href='http://%s/%s' target='_blank'>student%s</a></td>"%(exip,str(i[4]),s
+tr(i[0])))
 # get post data
-	form = cgi.FieldStorage()
-	os.system("cd /var/www/html/"+str(i[4])+"")
-	firstfile=os.system("find . -name 'php_echo.php'")
-	if firstfile==1:
-		print("<th>yes</th>")
-	else:
-		print("<th>no</th>")
-	os.system("cd /var/www/html/"+str(i[4])+"/course")
-	secfile=os.system("find . -name 'view_mod.php'")
-	if secfile ==1:
-		print("<th>ok</th>")
-	else:
-		print("<th>no</th>")
+        form = cgi.FieldStorage()
+        a=os.path.exists("/var/www/html/%s/php_echo.php"%str(i[4]))
+        if a==True:
+                print("<th>ok</th>")
+        else:
+                print("<th>no</th>")
+        b=os.path.exists("/var/www/html/%s/course/view_mod.php"%str(i[4]))
+        if b ==True:
+                print("<th>ok</th>")
+        else:
+                print("<th>no</th>")
 print("</tr></table>")
 print("""
 <form action="../login.html" method="GET">
     <input type="submit" value="Back to progress">
 </form>
 """)
+print("<td><button id='btn'>phpmyadmin</button></td>")
+print("<script type='text/javascript'>")
+print("var btn = document.getElementById('btn')")
+print("btn.onclick=function(){window.location.href='http://%s/phpMyAdmin'}"%exip)
+print("</script>")
+print("<h3>You can check all the changes in their moodle site</h3>")
 print('</center></body>')
